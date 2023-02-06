9ad6651bef87809899d6b3a6643f60f79bbc4286
maria.esquivel <maria.esquivel@globant.com>
2023-02-06 00:53:22 -0500

    refactor: update api server config

---
+++ api/server.js
@@ -0,0 +1,17 @@
+import jsonServer from "json-server";
+const server = jsonServer.create();
+const router = jsonServer.router("api/db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(jsonServer.bodyParser);
+server.use((req, _, next) => {
+  if (req.method === "POST") {
+    req.body.id = Date.now();
+  }
+  next();
+});
+server.use(middlewares);
+server.use(router);
+server.listen(3000, () => {
+  console.log("JSON Server is running");
+});
